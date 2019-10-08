9bc883f013211281a9602b4792e5b4f00cbb23e6
wangyfb <1939749829@qq.com>
2019-10-08 11:01:49 +0800

    Demo

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel=GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting:" + reply.Message);
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
