1b44992ce4e815e34e33e5174ceed66390a08eee
wangyfb <1939749829@qq.com>
2019-10-08 14:08:22 +0800

    add proto

---
+++ GrpcGreeter/Services/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using GrpcEmployee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcGreeter.Services
+{
+    public class EmployeeService: Employee.EmployeeBase
+    {
+        public override Task<EmpModel> getInfo(EmpRequest request, ServerCallContext context)
+        {
+            if (request.Id == "2")
+            {
+                return Task.FromResult(new EmpModel { Fid = "222", Name = "张三" });
+            }
+            else
+            {
+                return Task.FromResult(new EmpModel { Fid = "33333", Name = "XXX" });
+            }
+        }
+    }
+}
--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Grpc.Net.Client;
+using GrpcEmployee;
 using GrpcGreeter;
 using System;
 
@@ -8,12 +9,23 @@ namespace GrpcGreeterClient
     {
         static void Main(string[] args)
         {
-            var channel=GrpcChannel.ForAddress("https://localhost:5001");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
             var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting:" + reply.Message);
+            string id = "0";
+            while (id != "3")
+            {
+
+                id = Console.ReadLine();
+                var empClient = new Employee.EmployeeClient(channel);
+                var emp = empClient.getInfo(new EmpRequest { Id = id });
+
+                Console.WriteLine(emp);
+            }
+
+
             Console.ReadKey();
-            Console.WriteLine("Hello World!");
         }
     }
 }
